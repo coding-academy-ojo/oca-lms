8ec8cf7575f48d38c8f815e83b227fec51894dd6
rwabusini <rwabusini@gmail.com>
2024-07-22 12:01:06 +0300

    create masterpiece_deliverables column, add nullable for status in assignments submission table

--- database/migrations/2024_01_16_142452_create_student_table.php
+++ database/migrations/2024_01_16_142452_create_student_table.php
@@ -69,7 +69,7 @@ class CreateStudentTable extends Migration
             $table->string('linkedin_link')->nullable();
             $table->foreignId('academy_id')->constrained()->onDelete('cascade');
             $table->foreignId('cohort_id')->constrained()->onDelete('cascade');
-            $table->string('certificate_type')->default(0);
+            $table->string('certificate_type')->nullable();
             $table->boolean('internship_status')->default(0);
             $table->unsignedBigInteger('staff_id')->nullable();
             $table->foreign('staff_id')->references('id')->on('staff')->onDelete('cascade');
