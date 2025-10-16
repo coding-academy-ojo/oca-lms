1594a70a68449f5a8ea0491c97b33b549c2a8656
engreememad <Reememad_1991@yahoo.com>
2025-10-16 16:55:32 +0300

    final

--- resources/views/pdf/cohort-attendance-overall.blade.php
+++ resources/views/pdf/cohort-attendance-overall.blade.php
@@ -108,22 +108,10 @@
     </style>
 </head>
 <body>
-@php
-$path = public_path('images/Coding-Academy-LOGO-CMYK-Black-1024x576.png');
-$type = pathinfo($path, PATHINFO_EXTENSION);
-$data = file_get_contents($path);
-$base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
-@endphp
-
-<div class="header">
-    <img src="{{ $base64 }}" alt="Logo">
-    <h1>Cohort Summary Report</h1>
-</div>
     <!-- HEADER -->
     <div class="header">
         <img src="{{ asset('img/Coding-Academy-LOGO-CMYK-Black-1024x576.png') }}" alt="Logo">
         <h1>Cohort Summary Report</h1>
-        <h1>images/Coding-Academy-LOGO-CMYK-Black-1024x576.png</h1>
     </div>
 
     <!-- COHORT DETAILS -->
