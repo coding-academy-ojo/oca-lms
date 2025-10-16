c19aeaf7ce0da4c54642cec6dfef7b92223ea8c6
engreememad <Reememad_1991@yahoo.com>
2025-10-16 16:46:17 +0300

    new2

--- resources/views/pdf/cohort-attendance-overall.blade.php
+++ resources/views/pdf/cohort-attendance-overall.blade.php
@@ -111,9 +111,9 @@
 
     <!-- HEADER -->
     <div class="header">
-        <img src="{{ public_path('./images/Coding-Academy-LOGO-CMYK-Black-1024x576.png') }}" alt="Logo">
+        <img src="{{ public_path('images/Coding-Academy-LOGO-CMYK-Black-1024x576.png') }}" alt="Logo">
         <h1>Cohort Summary Report</h1>
-        <h1>./images/Coding-Academy-LOGO-CMYK-Black-1024x576.png</h1>
+        <h1>images/Coding-Academy-LOGO-CMYK-Black-1024x576.png</h1>
     </div>
 
     <!-- COHORT DETAILS -->
